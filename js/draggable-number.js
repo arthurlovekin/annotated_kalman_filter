c1761e583cf52b96d538f00fd3ffb158aef4af3a
arthurlovekin <arthurlovekin@gmail.com>
2024-10-07 16:52:09 -0700

    use setter more effectively (this breaks some examples)

--- js/draggable-number.js
+++ js/draggable-number.js
@@ -2,6 +2,8 @@ class DraggableNumber extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
+        this.isValidChange = () => true; // Default to always valid
+        this._value = 0;
     }
 
     connectedCallback() {
@@ -45,36 +47,39 @@ class DraggableNumber extends HTMLElement {
         this.valueDisplay = this.shadowRoot.querySelector('.draggable-txt');
         this.slider = this.shadowRoot.querySelector('.draggable-slider');
 
-        this.slider.addEventListener('input', this.updateValue.bind(this));
-        
+        this.slider.addEventListener('input', (event) => {
+            this.value = parseFloat(event.target.value); // This will trigger the setter
+        });
+
         // Set initial value
         this.value = parseFloat(initialValue);
     }
 
-    updateValue() {
-        this.value = parseFloat(this.slider.value);
-        this.valueDisplay.textContent = this.value.toFixed(2);
-        this.dispatchEvent(new CustomEvent('change', { detail: { value: this.value } }));
+    setValidityCondition(isValidChangeFunction) {
+        if (typeof isValidChangeFunction === 'function') {
+            this.isValidChange = isValidChangeFunction;
+        }
     }
 
     get value() {
-        return parseFloat(this.slider.value);
+        return this._value;
     }
 
     set value(newValue) {
-        const min = parseFloat(this.slider.min);
-        const max = parseFloat(this.slider.max);
-        const v = Math.min(Math.max(parseFloat(newValue), min), max);
-        if (this.valueDisplay) {
-            this.valueDisplay.textContent = v.toFixed(2);
-        }
-        if (this.slider) {
-            this.slider.value = v;
+        const clampedValue = Math.max(this.slider.min, Math.min(newValue, this.slider.max));
+        if(this.isValidChange(clampedValue)) {
+            this._value = clampedValue;
+            this.slider.value = clampedValue;
+            this.valueDisplay.textContent = clampedValue.toFixed(2);
+            this.dispatchEvent(new CustomEvent('change', { detail: { value: clampedValue } }));
         }
     }
 }
 
 customElements.define('draggable-number', DraggableNumber);
 
-// Helper function to get DraggableNumber instances
-window.getDraggable = (id) => document.getElementById(id).querySelector('draggable-number');
+// TODO: Remove all references to getDraggable
+// // Helper function to get DraggableNumber instances
+// //First document.getElementById(id) finds the element with the specified id. 
+// // Then, .querySelector() finds the first <draggable-number> custom element within that element.
+// window.getDraggable = (id) => document.getElementById(id).querySelector('draggable-number');
