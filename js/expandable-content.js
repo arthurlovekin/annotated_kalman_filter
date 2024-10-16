9bf80a8cb697e1a267ba35989eaa6f2c70ee92dc
arthurlovekin <arthurlovekin@gmail.com>
2024-10-16 12:36:57 -0700

    first pass at "option1" option2" buttons

--- js/expandable-content.js
+++ js/expandable-content.js
@@ -8,3 +8,24 @@ function toggleExpandableContent(id) {
     content.style.display = 'none';
   }
 }
+
+function activateButtonsAndContent(onButtonIds, onContentIds, offButtonIds, offContentIds) {
+  onButtonIds.forEach(id => {
+    const button = document.getElementById(id);
+    button.classList.add('active-button');
+    button.classList.remove('inactive-button');
+  });
+  offButtonIds.forEach(id => {
+    const button = document.getElementById(id);
+    button.classList.remove('active-button');
+    button.classList.add('inactive-button');
+  });
+  onContentIds.forEach(id => {
+    const content = document.getElementById(id);
+    content.style.display = 'block';
+  });
+  offContentIds.forEach(id => {
+    const content = document.getElementById(id);
+    content.style.display = 'none';
+  });
+}
