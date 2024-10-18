4b2ca6c17ee1058bd837d7b9850d2dd77c42b424
arthurlovekin <arthurlovekin@gmail.com>
2024-10-18 16:23:53 -0700

    removed old formula-element and separated FormulaElement class from definitions

--- js/formula-element-definitions.js
+++ js/formula-element-definitions.js
@@ -1,21 +1,3 @@
-// Holds all of the information (eg. display name, description, etc.) for a variable, 
-// piece of formula, or entire formula
-class FormulaElement {
-    constructor(div_name,
-                display_name, 
-                description, 
-                example, 
-                dimension,
-                color = 'rgba(255, 255, 0, 0.8)') {
-        this.div_name = div_name; // eg. predict-step-state
-        this.display_name = display_name; // What IS it? Name + matrix/vector/equation
-        this.description = description;  // What does it variable DO?
-        this.example = example;
-        this.dimension = dimension;
-        this.color = color;
-    }
-}
-
 const variables = {
     state: new FormulaElement("state", 
         "Mean State Vector",
--- js/formula-element.js
+++ js/formula-element.js
@@ -5,47 +5,13 @@ class FormulaElement {
                 display_name, 
                 description, 
                 example, 
-                convention_to_latex,
                 dimension,
-                color = 'rgba(255, 255, 0, 0.8)',
-                child_elements = {}) {
+                color = 'rgba(255, 255, 0, 0.8)') {
         this.div_name = div_name; // eg. predict-step-state
         this.display_name = display_name; // What IS it? Name + matrix/vector/equation
         this.description = description;  // What does it variable DO?
         this.example = example;
-        this.convention_to_latex = convention_to_latex; // eg. alt0 (affects how string is rendered)
-        this.convention = Object.keys(convention_to_latex)[0];
         this.dimension = dimension;
         this.color = color;
-        this.child_elements = child_elements;
-    }
-
-    update_naming_convention(convention) {
-        if (this.convention !== 'all') {
-            if (this.convention_to_latex.hasOwnProperty(convention)) {
-                this.convention = convention;
-            }
-            else {
-                console.log(`Naming convention ${convention} not found for ${this.div_name}`);
-            }
-        }
-        for (const child of Object.values(this.child_elements)) {
-            if (child instanceof FormulaElement) {
-                child.update_naming_convention(convention);
-            }
-            else {
-                console.log(`update_naming_convention(): ${this.div_name} has a child that is not a FormulaElement: ${child}`);
-            }
-        }
-    }
-
-    str(delimiter="") {
-        const string_fn = this.convention_to_latex[this.convention];
-        if (delimiter === "inline") {
-            return `\\(\\class{${this.div_name}}{${string_fn(this.child_elements)}}\\)`;
-        }
-        else {
-            return `${delimiter}\\class{${this.div_name}}{${string_fn(this.child_elements)}}${delimiter}`;
-        }
     }
 }
